97f99faa5c18bb03f26a6f1c8dd31f3ba6464e65
Ryan William O'Hara <ryan@ryanwohara.com>
2026-02-13 19:37:38 -0700

    feat: -colors set persists

--- src/author/cache.rs
+++ src/author/cache.rs
@@ -1,7 +1,7 @@
 use crate::{database, Colors};
 use moka::future::Cache;
 use mysql::params;
-use mysql::prelude::Queryable;
+use mysql::prelude::{Queryable, TextQuery};
 use once_cell::sync::Lazy;
 use std::time::Duration;
 
@@ -43,11 +43,30 @@ pub async fn get(author_host: String) -> Colors {
         .await
 }
 
-pub async fn set(author_host: String, color: Colors) {
-    CACHE.insert(author_host, color).await;
-}
+pub async fn set(author_host: String, colors: Colors) {
+    let c1 = colors.c1.to_string();
+    let c2 = colors.c2.to_string();
 
-#[allow(dead_code)]
-pub async fn invalidate(author_host: String) {
-    CACHE.invalidate(&author_host).await;
+    CACHE.insert(author_host.to_string(), colors).await;
+
+    let mut conn = match database::connect() {
+        Ok(conn) => conn,
+        Err(e) => {
+            println!("Error connecting to database: {}", e);
+            return;
+        }
+    };
+
+    let statement = match conn.exec_first::<(String, String), &str, mysql::Params>(
+        "SELECT color1, color2 FROM colors WHERE host = :author_host",
+        params! { "author_host" => author_host.to_string() },
+    ) {
+        Ok(Some(_)) => "UPDATE colors SET color1 = :c1, color2 = :c2 WHERE host = :author_host",
+        Ok(None) | Err(_) => {
+            "INSERT INTO colors (host, color1, color2) values(:author_host, :c1, :c2)"
+        }
+    }
+    .to_string();
+
+    let _ = conn.exec::<bool, &str, mysql::Params>(&statement, params! { author_host, c1, c2 });
 }
